dbf673b68b322f01b82023895ec6554649c20005
Pugach-Nazar <144114339+Pugach-Nazar@users.noreply.github.com>
2023-09-10 16:52:25 +0300

    Add files via upload

---
+++ pr1/pr1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace pr1._2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Стійка послідовність!");
+            var rndNumberGenerator = new RNGCryptoServiceProvider();
+            var randomNumber = new byte[8];
+            rndNumberGenerator.GetBytes(randomNumber);
+            for (int i = 0; i < 10; i++)
+            {
+                rndNumberGenerator.GetBytes(randomNumber);
+                Console.WriteLine(Convert.ToBase64String(randomNumber));
+            }
+        }
+    }
+}
