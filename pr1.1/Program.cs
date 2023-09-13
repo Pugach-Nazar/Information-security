a8188c6c00dc7e6fef54eae14302a550791b90bc
Admin <Admin@DESKTOP-PLQAS77>
2023-09-13 15:00:41 +0300

    Add project files.

---
+++ pr1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pr1._1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random;
+            for (int j = 0; j<5; j++) {
+                random = new Random(124);
+                for (int i = 0; i < 5; i++)
+                {
+                    int rnd1 = random.Next(1, 20);
+                    Console.Write(rnd1);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
