40c8cdf15ec671953ba3d852085b6e909f918a90
Назар Пугач <qwertyasd0933@gmail.com>
2023-09-16 10:55:47 +0300

    2.1 mostly done

--- Lab2.1/Program.cs
+++ Lab2.1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.IO;
 
 namespace Lab._2._1
 {
@@ -8,17 +9,42 @@ namespace Lab._2._1
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Input data");
-            string data = Console.ReadLine();
+            //Console.WriteLine("Input data");
+            //string data = Console.ReadLine();
+            //Console.WriteLine(data);
+            //string key = "key";
+            //string decdata = Cryption(data, key);
+            //Console.WriteLine(Cryption(data, key));
+            //Console.WriteLine(Cryption(decdata, key));
+            string encData,key, data = ReadDataFromFile("./data.txt");
             Console.WriteLine(data);
 
+            //Console.Write("Input key:");
+            //key = Console.ReadLine();
 
-            string key = "key";
-            string decdata = Cryption(data, key);
-            Console.WriteLine(Cryption(data, key));
-            Console.WriteLine(Cryption(decdata, key));
+            key = "123";
+
+            encData = Cryption(data,key);
+
+            WriteDataInFile(encData, "./encData.dat");
+            Console.WriteLine(ReadDataFromFile("./encData.dat"));
+            Console.WriteLine(Cryption(ReadDataFromFile("./encData.dat"),key));
         }
-        static string Cryption(string data, string key)
+
+        static string ReadDataFromFile(string path) 
+        {
+            byte[] data = File.ReadAllBytes(path);
+            return Encoding.UTF8.GetString(data);
+        }
+        static void WriteDataInFile(string data, string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path)) 
+            {
+                writer.WriteLine(data);
+            }
+        }
+        
+        static string Cryption(string data, string key = "key")
         {
 
             byte[] Bkey = Encoding.UTF8.GetBytes(key);
