3048d54198d6a69cd991aed2e0ac606064085a03
Pugach-Nazar <qwertyasd0933@gmail.com>
2023-11-11 19:20:51 +0200

    Add project files.

---
+++ Lab6/AesEncryption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class AesEncryption
+    {
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        public byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+        static public void AesExample()
+        {
+            var aes = new AesEncryption();
+            var key = aes.GenerateRandomNumber(32);
+            var iv = aes.GenerateRandomNumber(16);
+            Console.Write("Input data:");
+            string original = Console.ReadLine();
+            var encrypted = aes.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = aes.Decrypt(encrypted, key, iv);
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+            Console.WriteLine("\n-------------------\n");
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,52 @@
+﻿using lab5._2;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Des Example:\n");
+            DesEncryption.DesExample();
+
+            Console.WriteLine("Triple Des Example:\n");
+            TripleDesEncryption.TripleDesExample();
+
+            Console.WriteLine("Aes Example:\n");
+            AesEncryption.AesExample();
+
+            Console.WriteLine("2-nd Task");
+            var aes = new AesEncryption();
+
+            var key = aes.GenerateRandomNumber(32);
+            Console.Write("Random key: ");
+            Console.WriteLine(Convert.ToBase64String(key) + "\n");
+
+            key = PBKDF2.HashPassword(key, PBKDF2.GenerateSalt(), 20 * 10000, key.Length);
+            Console.Write("Key after hashfunc: ");
+            Console.WriteLine(Convert.ToBase64String(key));
+
+            var iv = aes.GenerateRandomNumber(16);
+            Console.Write("Random iv: ");
+            Console.WriteLine(Convert.ToBase64String(iv) + "\n");
+            iv = PBKDF2.HashPassword(iv, PBKDF2.GenerateSalt(), 20 * 10000, iv.Length);
+            Console.Write("Iv after hashfunc: ");
+            Console.WriteLine(Convert.ToBase64String(iv));
+
+            Console.Write("Input key:");
+            string original = Console.ReadLine();
+            var encrypted = aes.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = aes.Decrypt(encrypted, key, iv);
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+        }
+    }
+    
+}
